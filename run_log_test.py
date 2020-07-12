40380ca5c622b54adb760ebb4ff90c2bab830e65
wxfgithubone <1845719332@qq.com>
2020-07-13 06:09:59 +0800

    优化沉余代码

--- run_log_test.py
+++ run_log_test.py
@@ -1,34 +1,9 @@
 from xf_test.tets_case.models.function import send_emil, new_report, now_time
 from HTMLTestRunner import HTMLTestRunner
 import unittest
-# def send_emil(file_new):
-#     """=========定义发送邮件函数==========="""
-#     f = open(file_new, 'rb')  # 打开文件
-#     mail_body = f.read()  # 读取
-#     f.close()  # 读取完关闭
-#     sender = '1845719332@qq.com'  # 发送的邮箱
-#     receiver = "652894729@qq.com"  # 接收邮箱
-#     smtp_server = "smtp.qq.com"  # 发送邮箱服务器
-#     username = "1845719332@qq.com"  # 发送邮箱用户/密码
-#     password = "fqijzfeesegjbjbd"
-#     msg = MIMEText(mail_body, 'html', 'utf-8')  # 邮件内容
-#     msg['Subject'] = Header('自动化测试报告', 'utf-8')  # 主题
-#     smtp = smtplib.SMTP_SSL(smtp_server, 465)  # 连接
-#     smtp.login(username, password)
-#     smtp.sendmail(sender, receiver, msg.as_string())  # 发送
-#     smtp.quit()
-#     print("邮件已发出！")
-
-# def new_report(testreport):
-#     """====查找最新的测试报告文件====="""
-#     lists = os.listdir(testreport)
-#     lists.sort(key=lambda fn: os.path.getatime(testreport + "\\" + fn))
-#     file_new = os.path.join(testreport, lists[-1])
-#     print(file_new)
-#     return file_new
 
 
-if __name__ == '__main__':  # 运行是是是
+if __name__ == '__main__':
     # filename = './xf_test/report/report/' + now_time() + 'result.html'  # 相对路径
     filename = 'F:\\xf_py\\conner\\learning_log\\xf_test\\report\\report\\' + now_time() + 'result.html'  # 绝对路径
     fp = open(filename, 'wb')
